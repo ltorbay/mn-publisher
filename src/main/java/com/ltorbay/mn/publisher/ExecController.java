958f807889b73cfd3f007b23b05826d98101f13a
ltorbay <louistorbay@gmail.com>
2019-09-25 22:59:35 +0200

    Init security - Oauth2 client

--- src/main/java/com/ltorbay/mn/publisher/ExecController.java
+++ src/main/java/com/ltorbay/mn/publisher/ExecController.java
@@ -3,18 +3,21 @@ package com.ltorbay.mn.publisher;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
 
+@Secured(SecurityRule.IS_AUTHENTICATED)
 @Controller("/exec")
 public class ExecController {
 
-	private final EntrypointProducer entrypointProducer;
+    private final EntrypointProducer entrypointProducer;
 
-	public ExecController(final EntrypointProducer entrypointProducer) {
-		this.entrypointProducer = entrypointProducer;
-	}
+    public ExecController(final EntrypointProducer entrypointProducer) {
+        this.entrypointProducer = entrypointProducer;
+    }
 
-	@Get()
-	public void execute(@QueryValue final String text) {
-		entrypointProducer.execute(text);
-	}
+    @Get()
+    public void execute(@QueryValue final String text) {
+        entrypointProducer.execute(text);
+    }
 }
